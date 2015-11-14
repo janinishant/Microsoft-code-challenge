f774af928d3be45344ecb914c210f826d27f4a39
Chrysovalantis Anastasiou <chrysovalantis.anastasiou@gmail.com>
2015-11-14 15:41:04 -0800

    UI Skeleton

---
+++ app/public/js/register.js
@@ -0,0 +1,33 @@
+/**
+ * Created by canastas on 14/11/15.
+ */
+
+$("#photo_url")
+    .on('change', function() {
+        var url = $(this).val();
+
+        if (url == "") {
+            $("#img").addClass("hidden");
+        }
+        else {
+            $("#img")
+                .removeClass("hidden")
+                .attr("src", url);
+        }
+    })
+    .bind('paste', function() {
+        var url = $(this).val();
+
+        if (url == "") {
+            $("#img").addClass("hidden");
+        }
+        else {
+            $("#img")
+                .removeClass("hidden")
+                .attr("src", url);
+        }
+    });
+
+$("#register_form").on('submit', function(e) {
+    e.preventDefault();
+});
