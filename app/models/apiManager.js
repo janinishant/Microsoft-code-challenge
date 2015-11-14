710ed664cb05dafc60fa0b2a2e48027784c08d6c
Nishant Jani <nishantjani10@gmail.com>
2015-11-14 14:59:30 -0800

    Adding skeletons to a working node server and api manager that makes calls to microsoft oxford api

---
+++ app/models/apiManager.js
@@ -0,0 +1,14 @@
+module.exports = {
+    addProfile: function(data, callback) {
+        //Call the oxford api
+
+        //When you get a response
+        callback(err, responseData);
+    },
+    identifyProfile: function(data, callback) {
+        //Call identify api
+
+        //when you get the response
+        callback(err, responseData);
+    }
+};
